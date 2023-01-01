d21abc329d34544471ed887e02066a63fcf76757
Aochocinco <achalurankar@gmail.com>
2023-01-01 20:09:33 +0530

    init commit

---
+++ force-app/main/default/aura/wordle/wordle.app
@@ -0,0 +1,3 @@
+<aura:application>
+    <c:app></c:app>
+</aura:application>	
---
+++ force-app/main/default/lwc/grid/grid.js
@@ -0,0 +1,5 @@
+import { LightningElement } from 'lwc';
+
+export default class Grid extends LightningElement {
+    
+}
